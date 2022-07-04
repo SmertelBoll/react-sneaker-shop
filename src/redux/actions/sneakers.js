4f4b033b2e67c4c38aeb5ab69db31811d5151917
Lyubomyr <sholop2113@gmail.com>
2022-07-04 23:51:18 +0300

    delete proxy for axios

--- src/redux/actions/sneakers.js
+++ src/redux/actions/sneakers.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-axios.defaults.baseURL = "http://localhost:3001"
+// axios.defaults.baseURL = "http://localhost:3001"
 
 export const fetchSneakers = () => (dispatch) => {
    dispatch(setLoaded(false))
