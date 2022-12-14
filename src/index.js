691fdb226362a2bb6cacd1cd9326766248ac3695
Lyubomyr <sholop2113@gmail.com>
2022-12-14 22:50:05 +0200

    deploy on github pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename='react-sneaker-shop'>
          <Provider store={store}>
             <App />
          </Provider>
